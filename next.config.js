36da2f3edeb1d457765f2c631f2059de0c6f5490
JM <jmhands@gmail.com>
2025-03-09 12:18:39 -0700

    host

--- next.config.js
+++ next.config.js
@@ -6,11 +6,7 @@ const nextConfig = {
   webpack: (config) => {
     config.externals = [...(config.externals || []), 'smartmontools'];
     return config;
-  },
-  // Add hostname configuration
-  hostname: '0.0.0.0',
-  // Optional: you might also want to specify the port
-  port: 3000,
+  }
 }
 
 module.exports = nextConfig 
